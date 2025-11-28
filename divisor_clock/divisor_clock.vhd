61f78623fe58f6cc796310b08b98b4ec246e2ec1
Seabrinhagemaplys <66424663+Seabrinhagemaplys@users.noreply.github.com>
2025-11-28 17:30:56 -0300

    Add divisor_clock VHDL implementation
    Implement a clock divider to generate a 1Hz signal from a 50MHz input clock.

---
+++ divisor_clock/divisor_clock.vhd
@@ -0,0 +1,37 @@
+-- Arthur Coronho Seabra Eiras, Lucas Araújo Campos Szuster, Murillo Kelvin
+-- Código do professor Marconi
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+
+entity divisor_clock is
+	port (
+		CLOCK_50MHz : in std_logic;
+		reset : in std_logic;
+		CLOCK_1Hz : out std_logic
+	);
+
+end entity;
+
+architecture rtl of divisor_clock is
+
+begin
+	process (CLOCK_50MHz,reset)
+		variable cnt : integer range 0 to 25000000;
+		variable clk : std_logic:='0';
+	begin
+		if (reset = '1') then
+			cnt := 0;
+		elsif (rising_edge(CLOCK_50MHz)) then
+			if (cnt = 25000000) then
+				clk := not clk;
+				cnt := 0;
+			else
+				cnt := cnt + 1;
+			end if;
+		end if;
+		
+		-- atribuição da saida
+		CLOCK_1Hz <= clk;
+	end process;
+end architecture;
